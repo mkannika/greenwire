09b6e423e2d3cb3c2001a7bf380e91944118a771
Kannika <mkannika.comsci@gmail.com>
2022-03-17 22:54:47 +0700

    init files

---
+++ elements/footer.php
@@ -0,0 +1,59 @@
+<footer>
+	<div class="footer-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<a href="/"><img src="assets/images/Greenwire-logo-footer.svg" alt="Greenwire Logo"></a>
+				</div>
+				<div class="col-md-8 d-flex align-items-center justify-content-end">
+					<nav>
+						<ul>
+							<li><a href="#">Strain</a></li>
+							<li><a href="#">Dispensaries</a></li>
+							<li><a href="#">Learn</a></li>
+							<li><a href="#">Product</a></li>
+							<li><a href="#">Pricacy & Terms</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<ul class="social-list d-flex">
+						<li><a href="#"><img src="assets/images/facebook-icon.svg" alt="facebook icon"></a></li>
+						<li><a href="#"><img src="assets/images/twitter-icon.svg" alt="twitter icon"></a></li>
+						<li><a href="#"><img src="assets/images/instagram-icon.svg" alt="instagram icon"></a></li>
+						<li><a href="#"><img src="assets/images/youtube-icon.svg" alt="youtube icon"></a></li>
+					</ul>
+					<p>© 2022 Greenwire. All Rights Reserved.</p>
+				</div>
+				<div class="col-md-8 d-flex flex-wrap justify-content-end">
+					<ul class="footer-nav">
+						<li><a href="#">CBD</a></li>
+						<li><a href="#">Product</a></li>
+						<li><a href="#">Body</a></li>
+						<li><a href="#">Plant</a></li>
+						<li><a href="#">History</a></li>
+						<li><a href="#">Law</a></li>
+						<li><a href="#">Content</a></li>
+					</ul>
+					<ul class="footer-nav">
+						<li><a href="#">Gear</a></li>
+						<li><a href="#">CBD Product</a></li>
+						<li><a href="#">Sweet</a></li>
+						<li><a href="#">Cloth</a></li>
+					</ul>
+					<ul class="footer-nav">
+						<li><a href="#">Terms of use</a></li>
+						<li><a href="#">Commercial terms of use</a></li>
+						<li><a href="#">Privacy policy</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ elements/header.php
@@ -0,0 +1,53 @@
+<header class="position-fixed w-100">
+	<div class="nav-top d-flex align-items-center">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<a href="/"><img src="assets/images/Greenwire-logo.svg" alt="Greenwire Logo"></a>
+				</div>
+				<div class="col">
+					<div class="auth_button d-flex align-items-center justify-content-end">
+						<a href="#" class="d-flex align-items-center"><img src="assets/images/register.svg" alt="pin icon">Register</a>
+						<a href="#" class="d-flex align-items-center"><img src="assets/images/login.svg" alt="pin icon">Login</a>
+					</div>
+					<div class="user_profile d-flex align-items-center justify-content-end">
+						<div class="user_welcome d-flex align-items-center">
+							<img src="assets/images/profile-demo.png" alt="profile image">
+							<div class="welcome">
+								<p><span>Welcome</span>Domkrub</p>
+							</div>
+						</div>
+						<div class="user_address">
+							<p><img src="assets/images/pin.svg" alt="pin icon">Wang Thonglang, Bangkok</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="nav-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col d-flex">
+					<div class="hamburger-menu position-relative">
+						<span></span><span></span><span></span>
+					</div>
+					<form>
+						<input type="text" placeholder="Search, stores, Strains, Product. . .">
+						<button type="submit"><img src="assets/images/search.png" alt="search icon"></button>
+					</form>
+				</div>
+				<div class="col d-flex align-items-center justify-content-end">
+					<nav>
+						<ul>
+							<li><a href="#">Strain</a></li>
+							<li><a href="#">Dispensaries</a></li>
+							<li><a href="#">Learn</a></li>
+							<li><a href="#">Product</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>

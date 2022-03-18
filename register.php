b9c24888281e3bbd2b4c58522ed959311d4bd7f7
insawasd <mind1640@hotmail.com>
2022-03-18 15:52:25 +0700

    feat: register section

---
+++ register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Greenwire</title>
+	<link rel="stylesheet" href="assets/libs/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/global.css">
+	<link rel="stylesheet" href="assets/css/register.css">
+</head>
+<body>
+	<?php include 'elements/header.php'; ?>
+	<article>
+		<section class="section-register">
+			<div class="container">
+				<div class="row">
+					<div class="col col-group">
+						<div class="row">
+							<div class="col-md-6"></div>
+							<div class="col-md-6"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</article>
+	<?php include 'elements/footer.php'; ?>
+</body>
+</html>

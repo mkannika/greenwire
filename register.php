2799eb436236448a9db2f4b9d86da6e6821924af
insawasd <mind1640@hotmail.com>
2022-03-21 21:57:05 +0700

    feat: register page

--- register.php
+++ register.php
@@ -22,24 +22,56 @@
 								<p>เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16</p>
 							</div>
 						</div>
-						<div class="row justify-content-center">
+						<form action="" class="row justify-content-center">
 							<div class="col-md-6">
-								<div class="choose-image">
+								<div class="choose-image d-flex align-items-center">
 									<img src="assets/images/avatar.svg" alt="avatar">
 									<div class="choose-image-group">
-										<div class="choose-btn">เลือกรูป</div>
+										<div class="choose-btn d-flex align-items-center justify-content-center">เลือกรูป</div>
 										<p>ขนาดไฟล์: สูงสุด 1 MB</p>
 										<p>ไฟล์ที่รองรับ: .JPEG, .PNG</p>
 										<input type="file" name="profile" id="profile">
 									</div>
 								</div>
 							</div>
-							<div class="col-md-6"></div>
-						</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="User name *" class="form-control">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Name *" class="form-control">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Phone number *" class="form-control">
+							</div>
+							<div class="col-md-6">
+								<input type="password" placeholder="Password *" class="form-control">
+							</div>
+							<div class="col-md-6">
+								<input type="email" placeholder="Email *" class="form-control">
+							</div>
+							<div class="col-md-6">
+								<h3>หรือลงทะเบียนด้วย</h3>
+								<div class="social-auth d-flex">
+									<a href="#" class="social-btn d-flex align-items-center justify-content-center"><img src="assets/images/google.svg" alt="google"></a>
+									<a href="#" class="social-btn d-flex align-items-center justify-content-center"><img src="assets/images/facebook.svg" alt="facebook"></a>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-check">
+									<input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+									<label class="form-check-label" for="flexCheckDefault">
+										รับข้อมูลสิทธิพิเศษ และโปรโมชั่นจากลาซาด้า ผ่านทางอีเมล
+									</label>
+								</div>
+								<button type="submit">สมัครสมาชิก</button>
+								<p class="policy-content">โดยการกด "ลงทะเบียน" ข้าพเจ้าตกลงตาม <a href="#">ข้อกำหนดการใช้งาน</a> และ <a href="#">นโยบายความเป็นส่วนตัวของ</a> Greenwire</p>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
 		</section>
+		<?php include 'elements/welcome.php'; ?>
 	</article>
 	<?php include 'elements/footer.php'; ?>
 	<script src="assets/js/register.js"></script>

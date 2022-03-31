468302e44b68c490328a0d864d8270a1145bb720
insawasd <mind1640@hotmail.com>
2022-03-31 21:40:15 +0700

    feat: comment

--- review.php
+++ review.php
@@ -36,7 +36,19 @@
 					</div>
 					<div class="col col-content">
 						<h1>Reviews</h1>
-						
+						<div class="box-review">
+							<div class="title d-flex align-items-center justify-content-between">
+								<h3>Wedding Cake</h3>
+								<time>September 11, 2017</time>
+							</div>
+							<div class="comment">
+								<p>เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16 เมื่อเครื่องพิมพ์โนเนมเครื่องหนึ่งนำรางตัวพิมพ์มาสลับสับตำแหน่งตัวอักษรเพื่อทำหนังสือตัวอย่าง Lorem Ipsum อยู่ยงคงกระพันมาไม่ใช่แค่เพียงห้าศตวรรษ แต่อยู่มาจนถึงยุคที่พลิกโฉมเข้าสู่งานเรียงพิมพ์ด้วยวิธีทางอิเล็กทรอนิกส์ และยังคงสภาพเดิมไว้อย่างไม่มีการเปลี่ยนแปลง มันได้รับความนิยมมากขึ้นในยุค ค.ศ. 1960 เมื่อแผ่น Letraset วางจำหน่ายโดยมีข้อความบนนั้นเป็น Lorem Ipsum และล่าสุดกว่านั้น คือเมื่อซอฟท์แวร์การทำสื่อสิ่งพิมพ์ (Desktop Publishing) อย่าง Aldus PageMaker ได้รวมเอา Lorem Ipsum เวอร์ชั่นต่างๆ เข้าไว้ในซอฟท์แวร์ด้วย</p>
+								<div class="text-end">
+									<p class="helpful text-start">1072 people found this helpful</p>
+									<a href="#">Read full review</a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>

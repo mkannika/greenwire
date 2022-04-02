2f3511afb31b9c90a29fc046c7f3bbc4bf144d66
Kannika <mkannika.comsci@gmail.com>
2022-04-03 00:57:24 +0700

    feat : strain_detail - coding html part 1

---
+++ strain_detail.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greenwire</title>
+    <link rel="stylesheet" href="./assets/libs/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap-icons.css">
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <link rel="stylesheet" href="./assets/css/strain_detail.css">
+</head>
+
+<body>
+    <?php include 'elements/header.php'; ?>
+    <article>
+        <div class="container">
+            <!-- Breadcrumb -->
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item" aria-current="page">Home</li>
+                    <li class="breadcrumb-item">Strains</li>
+                    <li class="breadcrumb-item active">Wedding Cake</li>
+                </ol>
+            </nav>
+
+            <section class="main">
+                <h2>Wedding Cake</h2>
+                <p>aka Gorilla Glue, Original Glue, Gorilla Glue #4, Glue</p>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-favorite active"></div>
+                            <div class="card-img">
+                                <img src="./assets/images/strain/strain_01.jpg" alt="Image">
+                            </div>
+                            <div class="card-body">
+                                <div class="card-rating">
+                                    <div class="score-desc">
+                                        <span>Shop rating:</span>
+                                        <span class="rating-number">4.0</span>
+                                    </div>
+                                    <div class="star-rating">
+                                        <input id="box1-star-5" type="radio" name="rating" value="star-5" />
+                                        <label for="box1-star-5" title="5 stars">
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star"></i>
+                                        </label>
+                                        <input id="box1-star-4" type="radio" name="rating" value="star-4" />
+                                        <label for="box1-star-4" title="4 stars">
+                                            <i class="bi bi-star-fill active"></i>
+                                            <i class="bi bi-star"></i>
+                                        </label>
+                                        <input id="box1-star-3" type="radio" name="rating" value="star-3" />
+                                        <label for="box1-star-3" title="3 stars">
+                                            <i class="bi bi-star-fill active"></i>
+                                            <i class="bi bi-star"></i>
+                                        </label>
+                                        <input id="box1-star-2" type="radio" name="rating" value="star-2" />
+                                        <label for="box1-star-2" title="2 stars">
+                                            <i class="bi bi-star-fill active"></i>
+                                            <i class="bi bi-star"></i>
+                                        </label>
+                                        <input id="box1-star-1" type="radio" name="rating" value="star-1" />
+                                        <label for="box1-star-1" title="1 star">
+                                            <i class="bi bi-star-fill active"></i>
+                                            <i class="bi bi-star"></i>
+                                        </label>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="product-details-percent d-flex justify-content-between align-items-center">
+                            <div class="btn btn-default">Indica</div>
+                            <ul class="d-grid">
+                                <li class="d-flex align-items-center"><img src="./assets/images/home/icon_01.svg"
+                                        alt="THC"><span>THC 20%</span></li>
+                                <li class="d-flex align-items-center"><img src="./assets/images/home/icon_02.svg"
+                                        alt="CBG"><span>CBG 1%</span></li>
+                            </ul>
+                        </div>
+                        <div class="my-4">
+                            <div class="card-progress">
+                                <div class="d-flex justify-content-between">
+                                    <span>สงบ</span>
+                                    <span>มีพลัง</span>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25"
+                                        aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div class="card-progress">
+                                <div class="d-flex justify-content-between">
+                                    <span>THC ต่ำ</span>
+                                    <span>THC สูง</span>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50"
+                                        aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-details-tags row">
+                            <div class="col-6">
+                                <button class="btn btn-outline d-flex justify-content-center align-items-center w-100"><img src="./assets/images/home/icon_flower.svg" alt="THC"><div class="d-flex flex-column text-start mx-2"><span class="text-green">รสและกลิ่น</span><span>ดอกไม้</span></div></button>
+                            </div>
+                            <div class="col-6">
+                                <button class="btn btn-outline d-flex justify-content-center align-items-center w-100"><img src="./assets/images/home/icon_sleep.svg" alt="THC"><div class="d-flex flex-column text-start mx-2"><span class="text-green">ผลข้างเคียง</span>ง่วง<span></span></div></button>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์
+                                    มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16
+                                    เมื่อเครื่องพิมพ์โนเนมเครื่องหนึ่งนำรางตัวพิมพ์มาสลับสับตำแหน่งตัวอักษรเพื่อทำหนังสือตัวอย่าง
+                                    Lorem Ipsum อยู่ยงคงกระพันมาไม่ใช่แค่เพียงห้าศตวรรษ
+                                    แต่อยู่มาจนถึงยุคที่พลิกโฉมเข้าสู่งานเรียงพิมพ์ด้วยวิธีทางอิเล็กทรอนิกส์
+                                    และยังคงสภาพเดิมไว้อย่างไม่มีการเปลี่ยนแปลง มันได้รับความนิยมมากขึ้นในยุค ค.ศ. 1960
+                                    เมื่อแผ่น Letraset วางจำหน่ายโดยมีข้อความบนนั้นเป็น Lorem Ipsum และล่าสุดกว่านั้น
+                                    คือเมื่อซอฟท์แวร์การทำสื่อสิ่งพิมพ์ (Desktop Publishing) อย่าง Aldus PageMaker
+                                    ได้รวมเอา Lorem Ipsum เวอร์ชั่นต่างๆ เข้าไว้ในซอฟท์แวร์ด้วย</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title mb-4">Wedding Cake growing info</h3>
+                                <p class="card-text">เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์
+                                    มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16
+                                    เมื่อเครื่องพิมพ์โนเนมเครื่องหนึ่งนำรางตัวพิมพ์มาสลับสับตำแหน่งตัวอักษรเพื่อทำหนังสือตัวอย่าง
+                                    Lorem Ipsum อยู่ยงคงกระพันมาไม่ใช่แค่เพียงห้าศตวรรษ
+                                    แต่อยู่มาจนถึงยุคที่พลิกโฉมเข้าสู่งานเรียงพิมพ์ด้วยวิธีทางอิเล็กทรอนิกส์
+                                    และยังคงสภาพเดิมไว้อย่างไม่มีการเปลี่ยนแปลง มันได้รับความนิยมมากขึ้นในยุค ค.ศ. 1960
+                                    เมื่อแผ่น Letraset วางจำหน่ายโดยมีข้อความบนนั้นเป็น Lorem Ipsum และล่าสุดกว่านั้น
+                                    คือเมื่อซอฟท์แวร์การทำสื่อสิ่งพิมพ์ (Desktop Publishing) อย่าง Aldus PageMaker
+                                    ได้รวมเอา Lorem Ipsum เวอร์ชั่นต่างๆ เข้าไว้ในซอฟท์แวร์ด้วย</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </article>
+    <?php include 'elements/footer.php'; ?>
+</body>
+
+</html>

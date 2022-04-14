fa8fc839c8895d776d20a36b3dde65ecf403a9c5
Kannika <mkannika.comsci@gmail.com>
2022-04-15 02:35:11 +0700

    feat : create learn_detail.php

---
+++ learn_detail.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greenwire</title>
+    <link rel="stylesheet" href="./assets/libs/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap-icons.css">
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <link rel="stylesheet" href="./assets/css/learn.css">
+
+    <!-- Swiper -->
+    <link rel="stylesheet" href="./assets/libs/swiper/swiper-bundle.min.css" />
+    <script src="./assets/libs/swiper/swiper-bundle.min.js"></script>
+</head>
+
+<body>
+    <?php include 'elements/header.php'; ?>
+    <article>
+        <div class="container">
+            <!-- Breadcrumb -->
+            <nav aria-label="breadcrumb">
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item" aria-current="page"><a href="./">Home</a></li>
+					<li class="breadcrumb-item active">Learn</li>
+				</ol>
+			</nav>
+
+            <section class="main">
+                <div class="section-slide-2">
+                    <!-- Slider main container -->
+                    <div class="swiper">
+                        <!-- Additional required wrapper -->
+                        <div class="swiper-wrapper">
+                            <!-- Slides -->
+                            <div class="swiper-slide">
+                                <div class="col-md-4 swiper-content">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h3 class="card-title text-wrap">How set and setting affect cannabis and psychedelic experiences</h3>
+                                            <p class="card-text">เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16</p>
+                                            <a href="" class="btn btn-outline">Learn More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="swiper-img"><img src="./assets/images/home/slide_02.jpg" alt="Slide"></div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="col-md-4 swiper-content">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h3 class="card-title text-wrap">Cannabis 101</h3>
+                                            <p class="card-text">เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16</p>
+                                            <a href="" class="btn btn-outline">Learn More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="swiper-img"><img src="./assets/images/home/slide_02.jpg" alt="Slide"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- If we need navigation buttons -->
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+                    </div>
+                </div>
+
+                <div class="row mt-5">
+                    <!-- Sidebar -->
+                    <?php include 'elements/sidebar_tags.php'; ?>
+
+                    <!-- Content -->
+                    <div class="col-md-9 learn-content">
+                        
+                    </div>
+                </div>
+            </section>
+        </div>
+        
+        <!-- Welcome -->
+        <section id="welcome" class="section-welcome border-top">
+            <div class="container">
+                <div class="text-center mb-0">
+                    <h2>Welcome to Greenwire</h2>
+                    <p>A great place to discover cannabis.</p>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">A weed community</h4>
+                                <p class="card-text">11,000+ articles. 5,000+ strains. 1.3mm+ reviews. Legalization
+                                    advocates with a social justice and equity mindset.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Making life easy</h4>
+                                <p class="card-text">Shop by strain, price, deals, dispensary, location, brand,
+                                    star-rating, or vibes. Find the best legal weed near you.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Buy local, everywhere</h4>
+                                <p class="card-text">Find the neighborhood dispensaries wherever you are. Place a
+                                    free online order, pay when you get it.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </article>
+    <?php include 'elements/footer.php'; ?>
+    <script src="./assets/js/learn.js"></script>
+</body>
+
+</html>
